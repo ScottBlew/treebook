5df0ab663b011fb8ce37d9a8e2942e24c8703d04
Scott Blew <scott@nowhe.re>
2013-10-23 13:34:15 -0700

    updated profile page and scoped statuses

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,14 @@
+
 class ProfilesController < ApplicationController
   def show
-  end
+    
+    @user = User.find_by_profile_name(params[:id])
+    if @user
+      @statuses = @user.statuses.to_a
+      render action: :show
+    else
+    render file: 'public/404', status: 404, format: [:html]
+    end
+  end  
 end
+
--- test/integration/custom_routes_test.rb
+++ test/integration/custom_routes_test.rb
@@ -1,19 +1,24 @@
 require 'test_helper'
 
   class CustomRoutesTest < ActionDispatch::IntegrationTest
-    test "test the /login route opens the login page" do 
+  test "test the /login route opens the login page" do 
     get '/signin'
     assert_response :success
   end
   
-    test "test the /logout route" do 
+  test "test the /logout route" do 
     get '/logout'
     assert_response :redirect
     assert_redirected_to '/'
   end
   
-    test "test the /signup route" do 
+  test "test the /signup route" do 
     get '/signup'
+    assert_response :success
+  end
+  
+  test "that a profile page works" do
+    get '/scottblew'
     assert_response :success
   end
 
